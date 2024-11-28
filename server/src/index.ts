7027d2c349fd1e16974dc191246e29f52916c4b0
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2024-11-28 15:10:48 +0700

    feat(client): add tailwincss

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,4 @@ const app = new Elysia()
   .listen(3000)
 ;
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`running at ${app.server?.hostname}:${app.server?.port}`);
