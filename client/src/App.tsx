46d577aaa7e48bc6686a76ae5d0e4db06628e20f
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2024-11-28 15:48:26 +0700

    fix(client)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Home.page";
 
 function App() {
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage/>} />
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
