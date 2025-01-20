deeba0a119639e0be60a1a34ce749b5da9c9b6ce
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-01-20 10:31:20 +0700

    feat(server): apply userApi

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,7 +1,8 @@
 import Elysia from "elysia";
+import userApi from "./features/user/api";
 
 const api = new Elysia({ prefix: "/api" })
-  .get("/", () => "test api")
+  .use(userApi)
 ;
 
 export default api;
