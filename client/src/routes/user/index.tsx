950e2ae5eabba6d33091183c48560c2c74693b6e
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-02-05 20:38:19 +0700

    refac(client): restructure

--- client/src/routes/user/index.tsx
+++ client/src/routes/user/index.tsx
@@ -1,6 +1,21 @@
-import { createFileRoute } from '@tanstack/react-router'
-import UserPage from '../../features/user/container/UserPage'
+import { createFileRoute } from "@tanstack/react-router";
+import { useUsers } from "@/features/user/logic/hooks";
+import UserList from "@/features/user/view/UserList";
 
-export const Route = createFileRoute('/user/')({
-  component: UserPage,
-})
+export const Route = createFileRoute("/user/")({
+  component: Page,
+});
+
+function Page() {
+
+  const { data: users } = useUsers();
+
+  if(!users)
+    return null;
+
+  return (
+    <section>
+      <UserList users={users}/>
+    </section>
+  );
+}
