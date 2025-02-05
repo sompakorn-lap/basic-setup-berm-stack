38c9eb2b061d1054d2ea142ceddd7ed34cf13161
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-02-05 20:37:35 +0700

    chore(client): setup path alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     viteReact()
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
   server: {
     proxy: {
       "/api": "http://localhost:3000"
