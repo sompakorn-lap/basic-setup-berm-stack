ab67600a21033b29c1496af3d5d8c080e5c3687f
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-02-10 12:17:19 +0700

    build(client): migrate to tailwind css 4

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from '@tailwindcss/vite';
 import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
-    viteReact()
+    viteReact(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
