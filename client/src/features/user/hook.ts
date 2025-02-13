f9ff35e0adf5c87b63436be0d6d310ff614bd0bf
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-02-13 21:03:56 +0700

    fix(client)

--- client/src/features/user/hook.ts
+++ client/src/features/user/hook.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 export function useUsers() {
   return useQuery({
     queryKey: ["user"],
-    queryFn: () => axios.get("/api/user/notfound").then((res) => res.data),
+    queryFn: () => axios.get("/api/user").then((res) => res.data),
     retry: false
   });
 }
