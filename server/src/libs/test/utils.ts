7139a65e8131c136e8d354916e6df28361a5a397
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-02-14 23:09:04 +0700

    feat(server): add Tester class

--- server/src/libs/test/utils.ts
+++ server/src/libs/test/utils.ts
@@ -1,6 +1,17 @@
 import Elysia from "elysia";
 
-export function getTestUrl<T extends string>(controller: Elysia<T>) {
-  const { prefix } = controller.config;
-  return `http://localhost${prefix}`;
+export class Tester<T extends string> {
+  private controller: Elysia<T>;
+  private prefix: string;
+
+  constructor(controller: Elysia<T>) {
+    this.controller = controller;
+    this.prefix = controller.config.prefix || "";
+  }
+
+  fetch(path: `/${string}`, init?: RequestInit) {
+    return this.controller.handle(
+      new Request(`http://localhost${this.prefix}${path}`, init)
+    );
+  }
 }
