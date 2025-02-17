6e7e5cc77da24fa101529f290b1c52228a214299
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-02-17 17:06:52 +0700

    feat(server): use json from .env

--- server/src/api.ts
+++ server/src/api.ts
@@ -3,6 +3,7 @@ import userApi from "./features/user/user.api";
 
 const api = new Elysia({ prefix: "/api" })
   .use(userApi)
+  .get("/json-in-env", () => JSON.parse(process.env.DATA as string))
 ;
 
 export default api;
