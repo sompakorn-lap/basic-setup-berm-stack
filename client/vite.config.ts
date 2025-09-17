951f0db6b0208fcf32ff79003245ca9a8ed2c655
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-09-17 12:58:53 +0700

    build(client): refac config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from '@vitejs/plugin-react'
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from '@tailwindcss/vite';
-import path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
-    viteReact(),
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
+    react(),
     tailwindcss()
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
+      "@": `${process.cwd()}/src`
     }
   },
   server: {
